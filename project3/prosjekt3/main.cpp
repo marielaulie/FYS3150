7f9f1e421366258b2fd736b24cb71ca6839d7c0b
marielaulie <54407324+marielaulie@users.noreply.github.com>
2019-10-16 18:38:06 +0200

    https://github.com/CompPhysics/ComputationalPhysics/blob/master/doc/Programs/LecturePrograms/programs/FortranLibrary/f90lib.f90
    https://github.com/CompPhysics/ComputationalPhysics/blob/master/doc/Programs/LecturePrograms/programs/FortranLibrary/f90lib.f90

--- project3/prosjekt3/main.cpp
+++ project3/prosjekt3/main.cpp
@@ -7,3 +7,14 @@ int main()
     cout << "Hello World!" << endl;
     return 0;
 }
+//må parallelisere
+//Prosjekt 4 kommer til å gå sakte - så mange løkker som mulig
+//Ønsker mest mulig samlinger
+//parallellisering og vektorisering ønsker vi
+//task parallelism
+//mpi openmp
+//distribuert minne
+//mbi tillater kommunikasjon mellom maskiner
+//openmp kun delt minne
+//MBI samling av prosesser
+//
