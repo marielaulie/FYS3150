7d1871646ab83daeac486e97fd1e041ba3df8bf7
marielaulie <54407324+marielaulie@users.noreply.github.com>
2019-10-17 12:29:39 +0200

    Added files

---
+++ Prosjekt3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <armadillo>
+#include "time.h"
+
+using namespace std;
+using namespace arma;
+
+
+int main()
+{
+
+
+} // end of function to evaluate
+
+//må parallelisere
+//Prosjekt 4 kommer til å gå sakte - så mange løkker som mulig
+//Ønsker mest mulig samlinger
+//parallellisering og vektorisering ønsker vi
+//task parallelism
+//mpi openmp
+//distribuert minne
+//mbi tillater kommunikasjon mellom maskiner
+//openmp kun delt minne
+//MBI samling av prosesser
+//h
